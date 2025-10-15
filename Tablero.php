56fc3bd1acd8eef5cecdc347d67cfa71f2da48b4
new-CarlosEst <c.estebandiez@gmail.com>
2025-10-15 17:17:54 +0200

    creado el constructor de tablero y los getters de filas y columnas

--- Tablero.php
+++ Tablero.php
@@ -4,6 +4,19 @@ class Tablero{
     private $columnas;
     private $array;
 
-    
+    public function __construct(int $filas = 1, int $columnas = 1) {
+        $this-> filas = $filas;
+        $this-> columnas = $columnas;
+        //TODO Mirar como hacer un array bidimensional
+        $this-> array = ["a"]["3"];
+    }
+
+    public function getFilas(){
+        return $this-> filas;
+    }
+
+    public function getColumnas (){
+        return $this-> columnas;
+    }
 }
 ?>
