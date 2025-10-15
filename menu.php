4da5cbf0f0fc8d5a935941e7a64c6bfbf3cc00fd
new-CarlosEst <c.estebandiez@gmail.com>
2025-10-15 17:34:46 +0200

    Creado la carpeta resources y la de estilos y modificado menu.php

--- menu.php
+++ menu.php
@@ -1,3 +1,17 @@
 <?php
     session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/resources/iconoPagina.png" type="image/png">
+    <link rel="stylesheet" href="/css/menu.css">
+    <title>Menu_LightsOut</title>
+</head>
+<body>
+    
+</body>
+</html>
