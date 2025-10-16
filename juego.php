cb5ee3eff60c031323289c654059ce0b0bddeaf6
new-CarlosEst <c.estebandiez@gmail.com>
2025-10-16 16:58:59 +0200

    Cambio cosas pero sigue imprimiendo todas las celdas en la misma fila

--- juego.php
+++ juego.php
@@ -4,7 +4,7 @@
     //!Importante La sesion no puede tener nada arriba el codigo php, tiene que estar en la primera linea
 ?>
 <?php 
-
+    //Codigo para cambiar el color
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Le meto estilos de bootstrap para despues -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link href="/css/juego.css" rel = "stylesheet">
+    <link rel="stylesheet" href="css/juego.css">
     <title>pantalla-juego</title>
 </head>
 <body>
@@ -32,27 +32,28 @@
             $tablero = new Tablero(4,4);
             imprimirJuego($tablero);
         ?>
-        
-        <table border="1" style="border-collapse: collapse;">
-            <?php
-                function imprimirJuego($tablero){
-                    //Saco el array
-                    $fil = $tablero->getFilas();
-                    $colum = $tablero->getColumnas();
-                    $miArray = $tablero->getArray();
-                    //TODO Se me imprime la tabla en una misma fila todas las celdas arreglarlo
-                    for ($i = 0; $i< $fil ; $i++){
-                        echo "<tr>";
-                        for ($j = 0; $j < $colum; $j++){
-                            $color = ($miArray[$i][$j] == 1) ? "#fcfcfc" : "#e9f23a";
-                            echo '<td style="background-color: ' . $color . ';">';
-                            echo '<button style="background-color:' . $color . ';"> hola</button></td>';
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+            <table border="1" style="border-collapse: collapse;">
+                <?php
+                    function imprimirJuego($tablero){
+                        //Saco el array
+                        $fil = $tablero->getFilas();
+                        $colum = $tablero->getColumnas();
+                        $miArray = $tablero->getArray();
+                        //TODO Se me imprime la tabla en una misma fila todas las celdas arreglarlo
+                        for ($i = 0; $i< $fil ; $i++){
+                            echo "<tr>";
+                            for ($j = 0; $j < $colum; $j++){
+                                $color = ($miArray[$i][$j] == 1) ? "#fcfcfc" : "#e9f23a";
+                                echo '<td style="background-color: ' . $color . ';">';
+                                echo '<button type="submit" name="celda" style="background-color:' . $color . ';"  "width:100%; height:100%; margin:0; padding:0;>hola</button></td>';
+                            }
+                            echo "</tr>";
                         }
-                        echo "</tr>";
                     }
-                }
-            ?>
-        </table>
+                ?>
+            </table>
+        </form>
     </div>
 </body>
 </html>
